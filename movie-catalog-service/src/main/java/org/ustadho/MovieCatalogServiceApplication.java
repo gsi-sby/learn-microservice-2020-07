5d6ed29f3e5d0814c040c01a743f9109f3e08c6c
Ustadho_1218 <ustadho@mitrais.com>
2020-07-15 10:47:54 +0700

    13 Using WebClient to make API calls

--- movie-catalog-service/src/main/java/org/ustadho/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/org/ustadho/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -15,5 +16,10 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
 	}
 }
--- movie-catalog-service/src/main/java/org/ustadho/resource/MoviceCatalogResource.java
+++ movie-catalog-service/src/main/java/org/ustadho/resource/MoviceCatalogResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.ustadho.model.CatalogItem;
 import org.ustadho.model.Movie;
 import org.ustadho.model.Rating;
@@ -20,6 +21,9 @@ public class MoviceCatalogResource {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
     @GetMapping("{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId")  String userId) {
         List<Rating> ratings = Arrays.asList(
@@ -28,8 +32,13 @@ public class MoviceCatalogResource {
         );
 
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-
+//            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
 
         })
