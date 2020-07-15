de3dbffd01e5dc2bcbb4ab7afc7efeee03be25db
Ustadho_1218 <ustadho@mitrais.com>
2020-07-15 09:20:06 +0700

    Building the Movie Info Service

--- movie-info-service/src/main/java/org/ustadho/movieinfoservice/resource/MovieResource.java
+++ movie-info-service/src/main/java/org/ustadho/movieinfoservice/resource/MovieResource.java
@@ -1,4 +1,17 @@
 package org.ustadho.movieinfoservice.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ustadho.movieinfoservice.model.Movie;
+
+@RestController
+@RequestMapping("movies")
 public class MovieResource {
+
+    @GetMapping("{id}")
+    public Movie getMovieInfo(@PathVariable("id") String id) {
+        return new Movie(id, "Test name");
+    }
 }
--- rating-data-service/src/main/java/org/ustadho/ratingdataservice/resources/MovieCatalogResource.java
+++ rating-data-service/src/main/java/org/ustadho/ratingdataservice/resources/MovieCatalogResource.java
@@ -1,4 +1,7 @@
 package org.ustadho.ratingdataservice.resources;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MovieCatalogResource {
 }
