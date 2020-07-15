5115f0c58582bfa3ebaf3becf7e36ca91f3c9d2e
Ustadho_1218 <ustadho@mitrais.com>
2020-07-15 10:54:30 +0700

    14 Making a call to the Ratings API

--- rating-data-service/src/main/java/org/ustadho/ratingdataservice/resources/RatingResource.java
+++ rating-data-service/src/main/java/org/ustadho/ratingdataservice/resources/RatingResource.java
@@ -6,11 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.ustadho.ratingdataservice.model.Rating;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("ratingsdata")
 public class RatingResource {
     @GetMapping("{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @GetMapping("users/{userId}")
+    public List<Rating> getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1", 4),
+                new Rating("2", 3)
+        );
+        return ratings;
     }
 }
